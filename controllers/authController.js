8981e37a167d38d11fb3f18001e5c4e147fa3397
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-28 15:56:58 +0430

    token creating :)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,44 @@
 const validator = require('validator');
+const jsonWebToken = require('jsonwebtoken');
 
 const User = require('../models/User');
 const Catch = require('../utils/catch');
 const AppError = require('../utils/AppErorr');
+
+const signToken = ({ _id }) => {
+    return jsonWebToken.sign(
+        {
+            id: _id
+        },
+        process.env.JSON_WEB_TOKEN_SECRET,
+        {
+            expiresIn: process.env.JSON_WEB_TOKEN_TIME
+        }
+    );
+};
+
+const sendToken = (user, statusCode, res) => {
+    const token = signToken(user);
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
+
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    });
+};
 
 exports.signUp = Catch(
     async (req, res, next) => {
@@ -12,11 +48,6 @@ exports.signUp = Catch(
         }
         const user = await User.create({email, password});
         user.password = undefined;
-        res.status(200).json({
-            status: 'success',
-            data: {
-                user
-            }
-        });
+        sendToken(user, 201, res);
     }
 );
